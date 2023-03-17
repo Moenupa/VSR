74bf0df6bc82f8ffa3c4498b2d8645e271a56d44
Zhang Chengchen <1343806044@qq.com>
2023-03-17 19:04:15 +0800

    cvt to frames

--- src/prep/cvt2frames.py
+++ src/prep/cvt2frames.py
@@ -1,9 +1,9 @@
 import os
 import cv2
+import glob
+import logging
 
 from src.config import Config
-
-config = Config()
 
 
 def video2frames(video_path: str,
@@ -14,7 +14,7 @@ def video2frames(video_path: str,
                  lq_size: tuple = (320, 180)):
     # get video name
     file = os.path.basename(video_path)
-    filename = os.path.splitext(file)[0]
+    filename, ext = os.path.splitext(file)
 
     # make sure lq and gt folders exists
     lq_path = os.path.join(lq_base, filename)
@@ -23,14 +23,23 @@ def video2frames(video_path: str,
     os.makedirs(gt_path, exist_ok=True)
 
     cap = cv2.VideoCapture(video_path)
+    if cap is None:
+        logging.error(f'|{video_path}| not found')
+
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
     # checking dimensions and read video metadata
     if height != 720 or width != 1280:
         logging.warning(
             f'|{video_path}| skipped, improper dimension: {(width, height)}')
         return
+    elif fps < 20:
+        logging.warning(
+            f'|{video_path}| skipped, improper FPS: {fps}')
+        return
+
     max_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     n_frames = min(n_frames, max_frames)
     count = 0
@@ -54,3 +63,10 @@ def video2frames(video_path: str,
     logging.info(
         f"|{video_path}| to frames [{start_idx},{start_idx + count}] from [0, {max_frames}]"
     )
+
+
+if __name__ == '__main__':
+    config = Config(stdout=False)
+    files = glob.glob('./out/youTB/*')
+
+    video2frames(files[0], 'data/ytb1k/lq', 'data/ytb1k/gt')
