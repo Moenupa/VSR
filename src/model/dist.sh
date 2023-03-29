b421019db98531e202d63ad67b0cd51847d14ec8
Moenupa <moenupa@outlook.com>
2023-03-29 20:27:53 +0800

    dist.sh update

--- src/model/dist.sh
+++ src/model/dist.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 
-CONFIG="C:/vsr/vsr/src/model/config_edvr.py"
-GPUS=1
-NNODES=${NNODES:-2}
-NODE_RANK=${NODE_RANK:-0}
-PORT=${PORT:-29500}
-MASTER_ADDR=${MASTER_ADDR:-"10.22.5.52"}
-
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch \
-    --nnodes=$NNODES \
-    --node_rank=$NODE_RANK \
-    --master_addr=$MASTER_ADDR \
-    --nproc_per_node=$GPUS \
-    --master_port=$PORT \
-    C:/vsr/mmediting/tools/train.py \
-    $CONFIG \
+    --nproc_per_node=1 \
+    --nnodes=2 \
+    --node_rank=0 \
+    --master_addr=10.22.5.52 \
+    --master_port=29998 \
+    mmediting/tools/train.py \
+    vsr/src/model/config_edvr.py \
     --seed 0 \
     --launcher pytorch ${@:3}
