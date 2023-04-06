4c5f6d95cdb2cfc09cd91cb21a8882cab1776121
Moenupa WANG <moenupa@outlook.com>
2023-04-06 21:18:19 +0800

    convertor update

--- src/prep/convertor.py
+++ src/prep/convertor.py
@@ -1,11 +1,13 @@
-import os
-import cv2
-import logging
 import glob
+import logging
+import multiprocessing
+import os
 import random
-from multiprocessing import Pool
 
-from src.config import Config
+import cv2
+import pandas as pd
+
+from src.config import Config, LogUtils
 
 _720P = (1280, 720)
 _360P = (640, 360)
@@ -17,7 +19,7 @@ def video2frames(video_path: str,
                  start_idx: int = -1,
                  n_frames: int = 100,
                  dry_run: bool = False,
-                 gt_size: tuple = _720P) -> str:
+                 gt_size: tuple = _720P, **kwargs) -> list:
     # get video id and format into output path
     # make sure lq and gt folders exists
     vid = os.path.splitext(os.path.basename(video_path))[0]
@@ -30,6 +32,7 @@ def video2frames(video_path: str,
     frame_size = (cap.get(cv2.CAP_PROP_FRAME_WIDTH),
                   cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     max_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
     # checking dimensions and read video metadata
     if frame_size != gt_size:
@@ -38,11 +41,10 @@ def video2frames(video_path: str,
         raise IOError(f'[{vid}] skipped, too short: {max_frames} < {n_frames}')
 
     count = 0
-    if start_idx < 0:
-        # try get n_frames from the middle, or from the start
-        start_idx = random.randint(0, max_frames - n_frames)
-
     if not dry_run:
+        if start_idx < 0:
+            # try get n_frames from the middle, or from the start
+            start_idx = random.randint(0, max_frames - n_frames)
         # reading frames and save them into lq and gt folders
         # store frames [start_idx, ); len = n_frames
         cap.set(cv2.CAP_PROP_POS_FRAMES, start_idx)
@@ -56,7 +58,7 @@ def video2frames(video_path: str,
             _, gt = cap.read()
             count += 1
     cap.release()
-    return f"[{vid}] -> frames [{start_idx},{start_idx + count}] from [0,{max_frames}]"
+    return [vid, max_frames, fps, start_idx, count]
 
 
 def clean(root: str):
@@ -65,24 +67,54 @@ def clean(root: str):
             os.rmdir(path)
 
 
-def convert(clips: list, out: str, config: Config, n_threads: int = 32):
-    with Pool(n_threads) as pool:
-        for path in clips:
-            pool.apply_async(
-                video2frames,
-                kwds={'video_path': path, 'out_base': out, 'dry_run': config.dry_run},
-                callback=lambda msg: Config.log(config.stdout, logging.INFO, msg, config.log_dir),
-                error_callback=lambda e: Config.log(config.stdout, logging.WARN, e, config.log_dir)
-            )
-        pool.close()
-        pool.join()
+def worker(i: int, d: dict, *args, **kwargs):
+    try:
+        d[i] = video2frames(*args, **kwargs)
+        vid, max_frames, fps, start_idx, count = d[i]
+        LogUtils.log(
+            kwargs['config'].stdout, logging.INFO,
+            f"[{vid}] -> frames [{start_idx},{start_idx + count}] from [0,{max_frames}]",
+            kwargs['config'].log_dir
+        )
+    except Exception as e:
+        LogUtils.log(
+            kwargs['config'].stdout, logging.WARNING,
+            f"[{i}] -> {e}",
+            kwargs['config'].log_dir
+        )
 
+
+def convert(clips: list, out: str, config: Config) -> pd.DataFrame:
     clean(f'{out}/lq')
     clean(f'{out}/gt')
 
+    manager = multiprocessing.Manager()
+    ret_dict = manager.dict()
+    jobs = []
+    for i, clip in enumerate(clips):
+        p = multiprocessing.Process(target=worker, kwargs={
+            'i': i,
+            'd': ret_dict,
+            'video_path': clip,
+            'out_base': out,
+            'start_idx': -1,
+            'n_frames': 100,
+            'dry_run': config.dry_run,
+            'gt_size': _720P,
+            'config': config
+        })
+        jobs.append(p)
+        p.start()
+
+    for proc in jobs:
+        proc.join()
+
+    return pd.DataFrame.from_dict(ret_dict, orient='index', columns=['vid', 'frames', 'fps', 'ex_start', 'extracted'])
+
 
 if __name__ == '__main__':
-    config = Config(stdout=False, dry_run=False)
+    config = Config(stdout=True, dry_run=True)
     # convert(glob.glob(f"data/download/*.mp4"), 'data/STM', config)
     videos = glob.glob(f"data/YT8M/*.mp4")
-    convert(videos, 'data/STM', config)
+    ret = convert(videos, 'data/STM', config)
+    print(ret)
